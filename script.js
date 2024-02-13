27c5d53db89e202e05b778e7b23229f289b35547
Bolu <omogbohunboluwatife@icloud.com>
2024-02-13 13:50:21 +0100

    hr

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ function opentab(tabname){
     for(tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");
     }
-          event.currentTarget.classList.add("active-links");
-          document.getElementById(tabname).classList.add("active-tab");
+    
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
 
-      }
+}
 
-      var sidemenu = document.getElementById("sidemenu");
+var sidemenu = document.getElementById("sidemenu");
 
-      function openmenu(){
-        sidemenu.style.right = "0";
-      }
-      function closemenu(){
-        sidemenu.style.right = "-200px";
-      }
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
